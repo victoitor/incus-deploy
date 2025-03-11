c544ef243fe3425d3ff2dd3d3a81556e6c957f79
victoitor <58741687+victoitor@users.noreply.github.com>
2025-03-11 13:56:44 -0300

    Update main.tf
    Signed-off-by: victoitor <58741687+victoitor@users.noreply.github.com>

--- terraform/baremetal-incus/main.tf
+++ terraform/baremetal-incus/main.tf
@@ -1,6 +1,6 @@
 resource "incus_project" "this" {
   name        = var.project_name
-  description = "Project used to test incus-deploy"
+  description = "Project used to test bug"
   config = {
     "features.images"          = false
     "features.networks"        = false
@@ -8,19 +8,6 @@ resource "incus_project" "this" {
     "features.profiles"        = true
     "features.storage.buckets" = true
     "features.storage.volumes" = true
-  }
-}
-
-resource "incus_network" "this" {
-  project     = incus_project.this.name
-  name        = "br-ovn-test"
-  description = "Network used to test incus-deploy (OVN uplink)"
-
-  config = {
-    "ipv4.address" = var.ovn_uplink_ipv4_address
-    "ipv4.nat"     = "true"
-    "ipv6.address" = var.ovn_uplink_ipv6_address
-    "ipv6.nat"     = "true"
   }
 }
 
@@ -46,107 +33,13 @@ resource "incus_profile" "this" {
   }
 
   device {
-    type = "disk"
-    name = "agent"
-
-    properties = {
-      "source" = "agent:config"
-    }
-  }
-
-  device {
     type = "nic"
     name = "eth0"
 
     properties = {
-      "network" = var.network
+      "network" = incus_network.this.name
       "name"    = "eth0"
     }
-  }
-
-  device {
-    type = "nic"
-    name = "eth1"
-
-    properties = {
-      "network" = incus_network.this.name
-      "name"    = "eth1"
-    }
-  }
-
-  device {
-    type = "disk"
-    name = "disk4"
-
-    properties = {
-      "pool"   = var.storage_pool
-      "io.bus" = "nvme"
-      "source" = incus_storage_volume.disk4.name
-    }
-  }
-}
-
-resource "incus_storage_volume" "disk1" {
-  for_each = var.instance_names
-
-  project      = incus_project.this.name
-  name         = "${each.value}-disk1"
-  description  = "First CEPH OSD drive"
-  pool         = var.storage_pool
-  content_type = "block"
-  config = {
-    "size" = "20GiB"
-  }
-}
-
-resource "incus_storage_volume" "disk2" {
-  for_each = var.instance_names
-
-  project      = incus_project.this.name
-  name         = "${each.value}-disk2"
-  description  = "Second CEPH OSD drive"
-  pool         = var.storage_pool
-  content_type = "block"
-  config = {
-    "size" = "20GiB"
-  }
-}
-
-resource "incus_storage_volume" "disk3" {
-  for_each = var.instance_names
-
-  project      = incus_project.this.name
-  name         = "${each.value}-disk3"
-  description  = "Local storage drive"
-  pool         = var.storage_pool
-  content_type = "block"
-  config = {
-    "size" = "50GiB"
-  }
-}
-
-resource "incus_storage_volume" "disk4" {
-  project      = incus_project.this.name
-  name         = "shared-disk"
-  description  = "Shared block storage"
-  pool         = var.storage_pool
-  content_type = "block"
-  config = {
-    "size"            = "50GiB"
-    "security.shared" = "true"
-  }
-}
-
-resource "incus_storage_volume" "disk5" {
-  for_each = var.instance_names
-
-  project      = incus_project.this.name
-  name         = "${each.value}-disk5"
-  description  = "Linstor drive"
-  pool         = var.storage_pool
-  content_type = "block"
-  config = {
-    "size" = "50GiB"
   }
 }
 
