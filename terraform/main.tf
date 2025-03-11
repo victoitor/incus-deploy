552b593d4b17f1ead42ff849eb9466f571b55212
victoitor <58741687+victoitor@users.noreply.github.com>
2025-03-11 13:47:09 -0300

    Update main.tf
    Signed-off-by: victoitor <58741687+victoitor@users.noreply.github.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,14 +12,3 @@ module "baremetal" {
   ovn_uplink_ipv4_address = var.ovn_uplink_ipv4_address
   ovn_uplink_ipv6_address = var.ovn_uplink_ipv6_address
 }
-
-module "services" {
-  source = "./services"
-
-  project_name   = "dev-incus-deploy-services"
-  instance_names = ["ceph-mds01", "ceph-mds02", "ceph-mds03", "ceph-mgr01", "ceph-mgr02", "ceph-mgr03", "ceph-rgw01", "ceph-rgw02", "ceph-rgw03"]
-  image          = "images:${var.incus_image}"
-
-  storage_pool = var.incus_storage_pool
-  network      = var.incus_network
-}
